5a4653f2c8ae42fc3b4d07bca0a4aa9b8d5f3413
Rshcaroline <rshcaroline@163.com>
2018-06-16 01:33:51 +0800

    add comments

--- 3D-FFD-in-VTK-master/BuildControlFramework.py
+++ 3D-FFD-in-VTK-master/BuildControlFramework.py
@@ -77,6 +77,11 @@ def sphereCallback(obj, event):
                 # 设置定义几何信息的mapper到这个actor里
                 # 在里 mapper的类型是vtkPolyDataMapper 也就是用类似点、线、多边形(Polygons)等几何图元进行渲染的
                 actorlist[count].SetMapper(mapperlist[count])
+                # vtkActor.GetProperty()->SetColor() not working for me
+                # ref: http://vtk.1045678.n5.nabble.com/vtkActor-GetProperty-gt-SetColor-not-working-for-me-td5722373.html
+                actorlist[count].GetMapper().ScalarVisibilityOff()
+                # 设置Actor的颜色 该方法用RGB值来设置一个Actor的红、绿、蓝分量的颜色 每个分量的取值范围从0到1
+                actorlist[count].GetProperty().SetColor(1.0, 1.0, 0)
                 # 使用renderer的方法AddActor()把要渲染的actor加入到renderer中去。
                 ren.AddActor(actorlist[count])
                 count = count + 1
@@ -86,8 +91,12 @@ def sphereCallback(obj, event):
 # create a rendering window and renderer
 # 为了渲染actor需要创建图形对象, 先创建vtkRenderer的实例ren
 ren = vtk.vtkRenderer()
+# 设置背景颜色
+# ren.SetBackground(1, 1, 1)
+
 # vtkRenderer的实例ren协调渲染窗口renWin的视口（viewport）的渲染过程
 renWin = vtk.vtkRenderWindow()
+renWin.SetSize(1024*2, 528*3)
 # 通过渲染窗口类的方法AddRenderer()把renderer和渲染窗口关联起来
 renWin.AddRenderer(ren)
 
@@ -105,19 +114,23 @@ for i in range(totalsphere):
     sphereWidget = vtk.vtkSphereWidget()
     # 渲染窗口交互器实例iren是一个3D的球状体widget
     sphereWidget.SetInteractor(iren)
+    # 从index对应到真实空间中的xyz坐标
     x, y, z = index2realworld(i)
     # 设置球状体在真实空间中的xyz坐标
     sphereWidget.SetCenter(x, y, z)
     # 设置球状体的半径大小
     sphereWidget.SetRadius(0.01)
-    # 设置填充球状体的表面
+    # 设置球面的颜色 仍然是通过GetProperty来获取属性并进行设置
+    sphereWidget.GetSphereProperty().SetColor(0, 1.0, 0)
+    # 设置填充球状体的表面 三种基本的属性设置方式：点方式，网格方式和面方式
     sphereWidget.SetRepresentationToSurface()
     # 要是没有这一行 球状体就不会显示出来了
     sphereWidget.On()
+    # 将球状体添加到球状体的列表中
     spherelist.append(sphereWidget)
 
 # draw lines
-# 画线的
+# 画线
 sourcelist = []
 mapperlist = []
 actorlist = []
@@ -128,32 +141,12 @@ for i in range(3 * totalsphere):
     # 创建actor对象（要渲染的对象） 
     actorlist.append(vtk.vtkActor())
 
-# The following code seems repeated with function sphereCallback(obj, event)
-# count=0
-# for i in range(totalsphere):
-#     x, y, z = index2xyz(i)
-#     if (x + y + z) % 2 == 0:
-#         n = neighbor(i)
-#         for j in n:
-#             x1, y1, z1 = spherelist[i].GetCenter()
-#             x2, y2, z2 = spherelist[j].GetCenter()
-#             sourcelist[count].SetPoint1(x1, y1, z1)
-#             sourcelist[count].SetPoint2(x2, y2, z2)
-#             # 输出通过方法SetInputConnection()设置为vtkPolyDataMapper对象的输入
-#             mapperlist[count].SetInputConnection(sourcelist[count].GetOutputPort())
-#             # 设置定义几何信息的mapper到这个actor里
-#             actorlist[count].SetMapper(mapperlist[count])
-#             # 使用renderer的方法AddActor()把要渲染的actor加入到renderer中去。
-#             ren.AddActor(actorlist[count])
-#             count=count+1
-
 # set interaction
 # 用户方法通过定义一个函数并将其作为参数传入AddObserver来定义
 # 添加Observer监听vtkRenderWindowInteractor里的事件，定义一系列回调函数（或命令）来实现交互。
 # 将GUI交互器与用户自定义的渲染交互窗口交互器的方法关联起来
 for i in range(totalsphere):
     spherelist[i].AddObserver("InteractionEvent", sphereCallback)
-
 
 # enable user interface interactor
 iren.Initialize()
